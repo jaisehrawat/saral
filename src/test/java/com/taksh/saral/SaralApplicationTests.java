3e32ac317eeff8b620304e91f13d8e779b365321
Suresh <jaisehrawat11@gmail.com>
2023-08-21 17:22:41 +0530

    initial Commit

---
+++ src/test/java/com/taksh/saral/SaralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taksh.saral;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaralApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
